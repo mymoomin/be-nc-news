e0a700e1375177f759cb77b7ec478449a43a6fda
mymoomin <20903941+mymoomin@users.noreply.github.com>
2024-10-14 16:33:21 +0100

    Return promise

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -23,7 +23,7 @@ exports.getArticles = (request, response, next) => {
 
 exports.getCommentsByArticleId = (request, response, next) => {
   const { article_id } = request.params;
-  fetchCommentsByArticleId(article_id)
+  return fetchCommentsByArticleId(article_id)
     .then((comments) => {
       response.status(200).send({ comments });
     })
