e3679568abbb8cf4c185b533b12a61e6277f55fd
mymoomin <20903941+mymoomin@users.noreply.github.com>
2024-10-14 11:15:26 +0100

    Implement GET `/api/topics` endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,33 @@
 const request = require("supertest");
 
 const app = require("../app.js");
+const db = require("../db/connection.js");
+const data = require("../db/data/test-data");
+const seed = require("../db/seeds/seed.js");
+
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  db.end();
+});
+
+describe("/api/topics", () => {
+  test("GET: 200 -- responds with an array of all topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((res) => {
+        const topics = res.body.topics;
+        expect(topics).toHaveLength(3);
+        topics.forEach((topic) => {
+          expect(topic).toHaveProperty("slug", expect.any(String));
+          expect(topic).toHaveProperty("description", expect.any(String));
+        });
+      });
+  });
+});
 
 describe("/api/*", () => {
   test("ANY: 404 -- responds with a 404 Not Found and appropriate error message for an unknown url and/or method", () => {
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { getTopics } = require("./controllers/topics.controllers");
 
 const app = express();
+
+app.get("/api/topics", getTopics);
 
 app.all("*", (request, response, next) => {
   response.status(404).send({ msg: "unknown endpoint" });
