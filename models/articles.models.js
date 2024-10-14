35516004808b01a8f1889de5fe9f204e370aef89
mymoomin <20903941+mymoomin@users.noreply.github.com>
2024-10-14 15:24:03 +0100

    (GET `/api/articles/:article_id/comments`) Simplify error handling

--- models/articles.models.js
+++ models/articles.models.js
@@ -34,48 +34,25 @@ exports.fetchArticleById = (article_id) => {
     });
 };
 
-/**
- * Returns all comments associated with a given article.
- * Responds with status 404 if the article does not exist.
- *
- * @param {number} article_id
- * @returns
- */
-exports.fetchCommentsByArticleId = (article_id) => {
-  /*
-  We need to reject if the article isn't in the database.
-  A simple way to do this would be to first get all comments for the article with
-  `db.query("SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;")`,
-  and then, if there aren't any, check whether the article exists with
-  `db.query("SELECT * FROM articles WHERE article_id = $1;")`.
-  This works but requires two queries.
-
-  We can use a right join on `article_id` to do it in one.
-  With this query, all existing articles will have at least one row, so we reject if there aren't any.
-  An article with no comments will have one row which is all nulls, so we return an empty array.
-  Otherwise, we return the response as normal.
-  */
+// rejects if the article doesn't exist
+const checkArticleExists = (article_id) => {
   return db
-    .query(
-      `SELECT
-        comments.*
-      FROM
-        comments
-        RIGHT JOIN articles ON articles.article_id = comments.article_id
-      WHERE
-        articles.article_id = $1
-      ORDER BY
-        comments.created_at DESC
-      ;`,
-      [article_id]
-    )
-    .then(({ rows: comments }) => {
-      if (!comments.length) {
+    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
+    .then(({ rows: articles }) => {
+      if (!articles.length)
         return Promise.reject({ status: 404, msg: "Article not found" });
-      }
-      if (comments.length === 1 && comments[0].comment_id === null) {
-        return [];
-      }
-      return comments;
     });
+};
+
+exports.fetchCommentsByArticleId = (article_id) => {
+  // `Promise.all` rejects if any promise rejects
+  return Promise.all([
+    db.query(
+      "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;",
+      [article_id]
+    ),
+    checkArticleExists(article_id),
+  ]).then(([{ rows: comments }]) => {
+    return comments;
+  });
 };
